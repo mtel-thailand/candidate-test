411fd5f45d3494f40ac18db2ee73494f8f34b7d2
Sittitep Tosuwan <sittitep.tosuwan@tempsquad.co>
2024-03-07 00:50:53 +0700

    chore: add client view

--- cinema/backend/src/app.ts
+++ cinema/backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import * as data from './data/seed.json';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- cinema/client/src/App.js
+++ cinema/client/src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data.data));
+  }, []);
+
+  const MovieList = () => {
+    return (
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  function onSearch(event) {
+    console.log(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Seach</h1>
+        <input type="text" placeholder="Search..." onChange={onSearch}/>
+        <h1>Movies</h1>
+        <MovieList />
       </header>
     </div>
   );
