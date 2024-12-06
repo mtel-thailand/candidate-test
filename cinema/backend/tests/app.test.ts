0088f42792f856575563fcbf9bb2c7cfa0099867
Sittitep Tosuwan <sittitep.tosuwan@tempsquad.co>
2024-12-06 14:05:04 +0700

    chore: fix test

--- cinema/backend/tests/app.test.ts
+++ cinema/backend/tests/app.test.ts
@@ -3,58 +3,58 @@ import app from "../src/app";
 import { directorRepository, movieRepository } from "../src/db/data-source";
 import { AppDataSource } from "../src/db/data-source";
 
+beforeAll(async () => {
+  await AppDataSource.initialize();
+
+  const directors = [
+    {
+      name: "Frank Darabont",
+    },
+    {
+      name: "Francis Ford Coppola",
+    },
+    {
+      name: "Christopher Nolan",
+    },
+  ];
+
+  await directorRepository.save(directors);    
+
+  const movies = [
+    {
+      title: "The Shawshank Redemption",
+      year: 1994,
+      runtime: 142,
+      genres: "Drama",
+      director_id: "1",
+      actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+    },
+    {
+      title: "The Godfather",
+      year: 1972,
+      runtime: 175,
+      genres: "Crime, Drama",
+      director_id: "2",
+      actors: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
+    },
+    {
+      title: "The Dark Knight",
+      year: 2008,
+      runtime: 152,
+      genres: "Action, Crime, Drama, Thriller",
+      director_id: "3",
+      actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+    },
+  ];
+
+  await movieRepository.save(movies);
+});
+
+afterAll(async () => {
+  await AppDataSource.dropDatabase();
+});
+
 describe("GET /movies", () => {
-  beforeAll(async () => {
-    await AppDataSource.initialize();
-
-    const movies = [
-      {
-        title: "The Shawshank Redemption",
-        year: 1994,
-        runtime: 142,
-        genres: "Drama",
-        director_id: "1",
-        actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
-      },
-      {
-        title: "The Godfather",
-        year: 1972,
-        runtime: 175,
-        genres: "Crime, Drama",
-        director_id: "2",
-        actors: "Marlon Brando, Al Pacino, James Caan, Richard S. Castellano",
-      },
-      {
-        title: "The Dark Knight",
-        year: 2008,
-        runtime: 152,
-        genres: "Action, Crime, Drama, Thriller",
-        director_id: "3",
-        actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
-      },
-    ];
-
-    await movieRepository.save(movies);
-
-    const directors = [
-      {
-        name: "Frank Darabont",
-      },
-      {
-        name: "Francis Ford Coppola",
-      },
-      {
-        name: "Christopher Nolan",
-      },
-    ];
-
-    await directorRepository.save(directors);
-  });
-
-  afterAll(async () => {
-    await AppDataSource.dropDatabase();
-  });
-
   it("should return 200 OK", async () => {
     const response = await request(app).get("/movies");
 
