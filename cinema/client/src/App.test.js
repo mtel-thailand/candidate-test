bf2fba7e729fd142194ebb941009ceb0cc206893
stt-mtel <96401906+stt-mtel@users.noreply.github.com>
2025-07-31 03:29:54 +0000

    chore: update fe test

--- cinema/client/src/App.test.js
+++ cinema/client/src/App.test.js
@@ -3,7 +3,7 @@ import App from "./App";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-it("show all 3 movies", async () => {
+it("CASE#1 show all 3 movies", async () => {
   render(<App />);
 
   expect(
@@ -13,17 +13,17 @@ it("show all 3 movies", async () => {
   expect(await screen.findByText("The Dark Knight")).toBeInTheDocument();
 });
 
-it("search for a movie", async () => {
+it("CASE#2 search for a movie", async () => {
   render(<App />);
 
   const searchInput = await screen.findByPlaceholderText("Search...");
-  userEvent.type(searchInput, "god");
+  userEvent.type(searchInput, "Shawshank");
 
-  expect(await screen.findByText("The Godfather")).toBeInTheDocument();
+  expect(await screen.findByText("The Shawshank Redemption")).toBeInTheDocument();
 
   await waitFor(() => {
     expect(
-      screen.queryByText("The Shawshank Redemption")
+      screen.queryByText("The Godfather")
     ).not.toBeInTheDocument();
   });
 
